373a4d5eb5635c4e1bfbc200b4a7c8c1a83e1a5b
Javier Brenlla <javier.brenlla@espossible.com>
2023-11-24 20:08:51 +0100

    cambios y ajustes

--- pivoteo_parcial_escalado.py
+++ pivoteo_parcial_escalado.py
@@ -1,26 +1,21 @@
-subscript = str.maketrans("0123456789", "₀₁₂₃₄₅₆₇₈₉")
-
-def recoger_matriz():
-    rows, cols = (4, 4)
-    matriz = []
-    for i in range(rows):
-        col = []
-        for j in range(cols):
-            elemento = "a" + str(i+1) + str(j+1)
-            num = input("Ingrese el valor del elemento {}: ".format(elemento.translate(subscript)))
-            if num == "":
-                num = 0
-            col.append(num)
-        matriz.append(col)
-    return matriz
+def solicitar_matriz():
+    resultado = []
+    for i in range(4):
+        columna = []
+        for x in range(4):
+            elemento = "a" + str(i+1) + str(x+1)
+            numero = input("Ingrese el valor del elemento a" + str(i+1) + str(x+1) + ": ")
+            if numero == "":
+                numero = 0
+            columna.append(numero)
+        resultado.append(columna)
+    return resultado
 
 def imprimir_matriz(matriz):
     for row in matriz:
         print(row)
 
 def pivoteo(matriz, i):
-    col = str(i+1)
-    elemento = ("a" + str(i+1) + str(i+1)).translate(subscript)
     pivote = '%.2f' % 0
     aux = ''
     for l in matriz[i:4]:
@@ -37,22 +32,22 @@ def pivoteo(matriz, i):
     return matriz
 
 def reducion_gaussiana(matriz, i):
-    for j in range(i+1, 4):
+    for x in range(i+1, 4):
         pivote = matriz[i][i]
-        constante = float(matriz[j][i]) / float(pivote)
-        for k in range(i,4):
-            matriz[j][k] = float(matriz[j][k]) - (constante * float(matriz[i][k]))
+        constante = float(matriz[x][i]) / float(pivote)
+        for y in range(i,4):
+            matriz[x][y] = float(matriz[x][y]) - (constante * float(matriz[i][y]))
             
-        if matriz[j].count(0) == 4:
+        if matriz[x].count(0) == 4:
             print("El sistema no tiene solución única")
-            print(matriz[j])
+            print(matriz[x])
             exit(1)
             
     return matriz
 
 
 if __name__ == '__main__':
-    matriz_inicial = recoger_matriz()
+    matriz_inicial = solicitar_matriz()
     
     for i in range(3):
         matriz_inicial = pivoteo(matriz_inicial, i)
